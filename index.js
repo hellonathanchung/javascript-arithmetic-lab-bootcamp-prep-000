dfd98343631e9215dc47884ef71d7ee902eb1fb3
Nathan Chung <chung.nathanh@gmail.com>
2020-05-26 22:38:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,8 +10,19 @@ function divideBy3() {
 
 divideBy3()
 
-console.log(number // 3.333333333335)
+console.log(number) // 3.333333333335
 
-add5();
+add5()
 
 console.log(number) // 8.333333333335
+
+// reset number
+number = 10
+
+add5()
+
+console.log(number) // 15
+
+divideBy3()
+
+console.log(number) // 5
